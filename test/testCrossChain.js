3547f044ede3da087f222c49d3828e251b73ccab
david <daveaneo@gmail.com>
2023-12-05 11:45:18 -0700

    more tests...

--- test/testCrossChain.js
+++ test/testCrossChain.js
@@ -6,44 +6,52 @@ const hre = require("hardhat");
 const { ethers } = require('ethers');
 const { expect } = require("chai");
 
+// these depend upon deployment address and chains used (see constants.ts)
 const ethereumContractAddress = '0x707dE55f7E38eA2c61C553666a0eba7f4cC2f4d5'
 const avalancheContractAddress = '0x8c5179dfec1C590C59E6607b73cfd0891fa59Bf5'
-const mountainBlockchainInfo = 16015286601757825753;
+const mountainChainSelector = ethers.BigNumber.from('16015286601757825753');
+const lakeChainSelector = ethers.BigNumber.from('14767482510784806043');
+
+const zeroAddress = "0x0000000000000000000000000000000000000000";
 
 
-//async function main() {
+const ethereumProvider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_SEPOLIA_RPC_URL);
+const polygonProvider = new ethers.providers.JsonRpcProvider(process.env.POLYGON_MUMBAI_RPC_URL);
+const optimismProvider = new ethers.providers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL);
+const arbitrumProvider = new ethers.providers.JsonRpcProvider(process.env.ARBITRUM_TESTNET_RPC_URL);
+const avalancheProvider = new ethers.providers.JsonRpcProvider(process.env.AVALANCHE_FUJI_RPC_URL);
+
+// Create a wallet instance
+const walletEth = new ethers.Wallet(process.env.PRIVATE_KEY, ethereumProvider);
+const walletAvax = new ethers.Wallet(process.env.PRIVATE_KEY, avalancheProvider);
+
+var mountain, lake;
+
+
+async function initialSetup() {
+  // Load the contract's artifacts, which includes the ABI
+  const mountainContract = await hre.artifacts.readArtifact("Mountain");
+  // Extract the ABI
+  const mountainAbi = mountainContract.abi;
+  mountain = new ethers.Contract(ethereumContractAddress, mountainAbi, walletEth);
+  lake = new ethers.Contract(avalancheContractAddress, mountainAbi, walletAvax);
+
+  // if mountainInfo not set on Lake, set it.
+  const {blockchainId, contractAddress} = await lake.getMountainInfo();
+  const invalidBlockchainId = 0;
+  const invalidContractAddress = '0x0000000000000000000000000000000000000000';
+
+  if (blockchainId === invalidBlockchainId || contractAddress === invalidContractAddress) {
+    const tx = await lake.setMountainInfo(mountainChainSelector, mountain.address);
+    await tx.wait(); // Wait for the transaction to be mined
+  }
+}
+
 describe("Mountain Contract", function () {
   // Setup providers for different networks
-  const ethereumProvider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_SEPOLIA_RPC_URL);
-  const polygonProvider = new ethers.providers.JsonRpcProvider(process.env.POLYGON_MUMBAI_RPC_URL);
-  const optimismProvider = new ethers.providers.JsonRpcProvider(process.env.OPTIMISM_GOERLI_RPC_URL);
-  const arbitrumProvider = new ethers.providers.JsonRpcProvider(process.env.ARBITRUM_TESTNET_RPC_URL);
-  const avalancheProvider = new ethers.providers.JsonRpcProvider(process.env.AVALANCHE_FUJI_RPC_URL);
-
-  var mountain, lake;
-
-  // Create a wallet instance
-  const walletEth = new ethers.Wallet(process.env.PRIVATE_KEY, ethereumProvider);
-  const walletAvax = new ethers.Wallet(process.env.PRIVATE_KEY, avalancheProvider);
 
     beforeEach(async function () {
-        // Load the contract's artifacts, which includes the ABI
-        const mountainContract = await hre.artifacts.readArtifact("Mountain");
-        // Extract the ABI
-        const mountainAbi = mountainContract.abi;
-        mountain = new ethers.Contract(ethereumContractAddress, mountainAbi, walletEth);
-        lake = new ethers.Contract(avalancheContractAddress, mountainAbi, walletAvax);
-
-        // if mountainInfo not set on Lake, set it.
-        const {blockchainId, contractAddress} = await lake.getMountainInfo();
-        const invalidBlockchainId = 0;
-        const invalidContractAddress = '0x0000000000000000000000000000000000000000';
-
-        if (blockchainId==invalidBlockchainId || contractAddress == invalidContractAddress){
-          const tx = await lake.setMountainInfo(mountainBlockchainInfo, mountain.address);
-        }
-
-
+        await initialSetup();
     });
 
     describe("Mountain setup is correct", function () {
@@ -101,20 +109,118 @@ describe("Mountain Contract", function () {
           expect(blockchainId).to.equal(expectedBlockchainId);
           expect(contractAddress).to.equal(expectedContractAddress);
       });
-
     });
 
+    describe("Stage and withdraw 'ETH' on Mountain", function () {
+          let tx = await mountain.stageLiquidity(zeroAddress,amountToStage, {value:amountToStage});
+          let txReceipt = await tx.wait();
 
-    describe("Bridging Works", function () {
+          let stagedAmount = await mountain.liquidityStaging(mountainChainSelector, zeroAddress)
+          tx = await mountain.withdrawStagedLiquidity(mountainChainSelector, stagedAmount);
 
-      it("Stage Eth on mountain", async function () {
-        expect(true).to.equal(true);
-      });
+          let finalStagedAmount = await mountain.liquidityStaging(mountainChainSelector, zeroAddress)
+          expect(finalStagedAmount).to.equal(0);
     });
+
+    describe("Stage and withdraw 'ETH' on Lake", function () {
+          let tx = await lake.stageLiquidity(zeroAddress,amountToStage, {value:amountToStage});
+          let txReceipt = await tx.wait();
+
+          let stagedAmount = await lake.liquidityStaging(lakeChainSelector, zeroAddress);
+
+          await expect(
+            lake.withdrawStagedLiquidity(lakeChainSelector, stagedAmount)
+          ).to.be.revertedWith("Withdrawal not approved or terrain type mismatch");
+    });
+
 
 });
-//
-//main().catch(error => {
-//  console.error(error);
-//  process.exit(1);
-//});
+
+
+
+describe("Full Bridging", function () {
+  // Setup providers for different networks
+
+    beforeEach(async function () {
+        await initialSetup();
+    });
+
+      const amountToStage = 100000;
+
+      describe("Stage ETH on Mountain", function () {
+        var stagedInit;
+        var tx;
+        var txReceipt;
+
+        beforeEach(async function () {
+        });
+
+
+        it("Expect LiquidityStaged to be emitted", async function () {
+          stagedInit = await mountain.liquidityStaging(mountainChainSelector, zeroAddress)
+          tx = await mountain.stageLiquidity(zeroAddress,amountToStage, {value:amountToStage});
+          txReceipt = await tx.wait();
+
+          // Check if the transaction receipt has the expected event
+          expect(txReceipt.events?.some((event) => event.event === "LiquidityStaged")).to.be.true;
+          //    event LiquidityStaged(uint256 indexed blockchainId, address indexed provider, address indexed token, uint256 amount);
+        });
+
+
+        it("Event values as expected", async function () {
+          // Additional check for event arguments if necessary
+          const event = txReceipt.events?.find((event) => event.event === "LiquidityStaged");
+          expect(event?.args?.blockchainId).to.equal(mountainChainSelector, 'blockchainId incorrect');
+          expect(event?.args?.provider).to.equal(walletEth.address, 'providerId incorrect');
+          expect(event?.args?.token).to.equal(zeroAddress, 'token incorrect');
+          expect(event?.args?.amount).to.equal(amountToStage, 'amount incorrect');
+        });
+
+        it("Expect 'LiquidityStaged' to be emitted", async function () {
+          const stagedFinal = await mountain.liquidityStaging(mountainChainSelector, zeroAddress)
+          expect(stagedInit.add(amountToStage)).to.equal(stagedFinal);
+        });
+      });
+
+      describe("Stage 'ETH' on Lake", function () {
+        var stagedInit;
+        var tx;
+        var txReceipt;
+
+        beforeEach(async function () {
+        });
+
+
+        it("Expect LiquidityStaged to be emitted", async function () {
+          stagedInit = await lake.liquidityStaging(lakeChainSelector, zeroAddress)
+          tx = await lake.stageLiquidity(zeroAddress,amountToStage, {value:amountToStage});
+          txReceipt = await tx.wait();
+
+          // Check if the transaction receipt has the expected event
+          expect(txReceipt.events?.some((event) => event.event === "LiquidityStaged")).to.be.true;
+          //    event LiquidityStaged(uint256 indexed blockchainId, address indexed provider, address indexed token, uint256 amount);
+        });
+
+
+        it("Event values as expected", async function () {
+          // Additional check for event arguments if necessary
+          const event = txReceipt.events?.find((event) => event.event === "LiquidityStaged");
+          expect(event?.args?.blockchainId).to.equal(lakeChainSelector, 'blockchainId incorrect');
+          expect(event?.args?.provider).to.equal(walletEth.address, 'providerId incorrect');
+          expect(event?.args?.token).to.equal(zeroAddress, 'token incorrect');
+          expect(event?.args?.amount).to.equal(amountToStage, 'amount incorrect');
+        });
+
+        it("Expect 'LiquidityStaged' to be emitted", async function () {
+          const stagedFinal = await lake.liquidityStaging(lakeChainSelector, zeroAddress)
+          expect(stagedInit.add(amountToStage)).to.equal(stagedFinal);
+        });
+      });
+
+
+      it("Staging on Mountain reflects both", async function () {
+        expect(true).to.equal(true);
+      });
+
+
+});
